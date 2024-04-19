4a6f5c9ef27cd80e79d9e5669619da22a45add0c
iboonkhet <iboonkhet@gmail.com>
2024-04-19 16:36:40 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,18 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+
+    mix.webpackConfig({
+        resolve: {
+            alias: {
+                "@environment" : path.resolve(__dirname,"resources/js/environment"),
+                "@components"  : path.resolve(__dirname,"resources/js/components"),
+                "@filters"     : path.resolve(__dirname,"resources/js/filters"),
+                "@services"    : path.resolve(__dirname,"resources/js/services"),
+                "@store"       : path.resolve(__dirname,"resources/js/store")
+            }
+        }
+    });
+    
