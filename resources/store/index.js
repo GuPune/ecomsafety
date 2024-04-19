6299582376447e9551d52dbe0d16edda3776ab4a
iboonkhet <iboonkhet@gmail.com>
2024-04-19 16:31:33 +0700

    Update index.js

--- resources/store/index.js
+++ resources/store/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Product from './modules/product.module'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    modules: {  },
+    modules: { Product },
 })
 
