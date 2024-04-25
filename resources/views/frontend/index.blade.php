b3999518918eb936d78a02d43e173f6bd073f87a
iboonkhet <iboonkhet@gmail.com>
2024-04-25 14:07:49 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -59,7 +59,7 @@
 
         <div class="row">
             <div class="col-12">
-                <div class="section-title-group">
+                <div class="section-title" style="margin-bottom: 5px;">
                     <h2>Group Item</h2>
                 </div>
             </div>
