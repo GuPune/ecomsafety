451b8f27bb29d4f4f7e4a449c552c4adcbbe8664
iboonkhet <iboonkhet@gmail.com>
2024-04-25 14:03:16 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -57,7 +57,13 @@
             </div>
         </div>
 
-
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Hot Item</h2>
+                </div>
+            </div>
+        </div>
         <div class="row">
 
             @if($group)
