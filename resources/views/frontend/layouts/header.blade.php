5e66ca62dde6a87485cce87a782a657a28ffffdd
iboonkhet <iboonkhet@gmail.com>
2024-05-25 12:57:42 +0700

    update header
    update header

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -124,6 +124,29 @@
                         <div class="sinlge-bar shopping">
                             <a href="{{route('cart')}}" class="single-icon"><i class="ti-bag"></i> <span class="total-count">{{Helper::cartCount()}}</span></a>
                             <!-- Shopping Item -->
+
+                            <div class="shopping-item">
+                                    <div class="dropdown-cart-header">
+                                        <span> Items</span>
+                                        <a href="{{route('wishlist')}}">View Wishlist</a>
+                                    </div>
+                                    <ul class="shopping-list">
+                                    
+                                                    <li>
+                                                        <a href="" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
+                                                        <a class="cart-img" href="#"></a>
+                                                        <h4><a href="" target="_blank"></a></h4>
+                                                    </li>
+                                          
+                                    </ul>
+                                    <div class="bottom">
+                                        <div class="total">
+                                            <span>Total</span>
+                                            <span class="total-amount">THB</span>
+                                        </div>
+                                        <a href="" class="btn animate">Cart</a>
+                                    </div>
+                                </div>
                             @auth
                                 <div class="shopping-item">
                                     <div class="dropdown-cart-header">
