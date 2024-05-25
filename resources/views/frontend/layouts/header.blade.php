1eef767043bcc74e361e97c8ab4918234bff10af
iboonkhet <iboonkhet@gmail.com>
2024-05-25 10:43:19 +0700

    Update header.blade.php

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -57,7 +57,7 @@
                         @php
                             $settings=DB::table('settings')->get();
                         @endphp                    
-                        <a href="{{route('home')}}"><img src="@foreach($settings as $data) {{$data->logo}} @endforeach" alt="logo"></a>
+                        <a href="{{route('home')}}"><img src="@foreach($settings as $data) {{$data->logo}} @endforeach" alt="logo" height="60px"></a>
                     </div>
                     <!--/ End Logo -->
                     <!-- Search Form -->
