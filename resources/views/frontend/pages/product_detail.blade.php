1422baf76baf60a354d4f04ab5a13ad5bd836592
iboonkhet <iboonkhet@gmail.com>
2024-06-17 13:35:34 +0700

    update
    update

--- resources/views/frontend/pages/product_detail.blade.php
+++ resources/views/frontend/pages/product_detail.blade.php
@@ -139,7 +139,7 @@
 													<!--/ End Input Order -->
 													</div>
 													<div class="add-to-cart mt-4">
-														<button type="submit" class="btn">Add to cart</button>
+													
 														<a href="{{route('add-to-wishlist',$product_detail->slug)}}" class="btn min"><i class="ti-heart"></i></a>
 													</div>
 											
